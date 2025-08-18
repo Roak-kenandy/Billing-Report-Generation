03c0ecfed7539234aa5900dede3299ee0502fd3c
Roak <Roak@Ahmeds-MacBook-Pro-2.local>
2025-08-18 11:24:33 +0500

    Added account number for customer reports

--- frontend/src/components/loginReport/CustomerReports.js
+++ frontend/src/components/loginReport/CustomerReports.js
@@ -40,8 +40,8 @@ const CustomerReports = () => {
     const [selectedIsland, setSelectedIsland] = useState('');
     const [appliedAtoll, setAppliedAtoll] = useState('');
     const [appliedIsland, setAppliedIsland] = useState('');
-    // const API_URL = 'https://mdnrpt.medianet.mv/billing-reports';
-    const API_URL = 'http://localhost:3003/billing-reports';
+    const API_URL = 'https://mdnrpt.medianet.mv/billing-reports';
+    // const API_URL = 'http://localhost:3003/billing-reports';
 
     useEffect(() => {
         const fetchAtolls = async () => {
